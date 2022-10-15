a68393f3c0f9f5699ab098d68c241124e084bb81
sergioeucnam <sergioeucnam@gmail.com>
2022-10-15 01:46:21 -0300

    update

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,6 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	base: '/pokeapi/',
+	base: '/',
 	plugins: [react()],
-	server: {
-		host: true,
-		port: 5173,
-	},
 });
